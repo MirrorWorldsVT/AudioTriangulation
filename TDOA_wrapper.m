6798c06542b648ebd86ad1d1abcd3b9ed278f013
wamanda7 <wamanda7@vt.edu>
2015-04-03 11:07:48 -0400

    added correct index calculations

--- TDOA_wrapper.m
+++ TDOA_wrapper.m
@@ -8,13 +8,17 @@ timediff = zeros(n*(n-1)/2 , 1);
 corr = zeros(n*(n-1)/2 , 1); 
 lag = zeros(n*(n-1)/2 , 1);
 
+ind = 1;
+
 for i = 1:(n-1)
     for j = (i+1):n
         audio_one = input_array(:,i);
         audio_two = input_array(:,j);
-        [timediff(i+(j-1)), corr(i+(j-1)), lag(i+(j-1))] = TDOA(audio_one, audio_two, 48000);
+        [timediff(ind), corr(ind), lag(ind)] = TDOA(audio_one, audio_two, 48000);
         %replace 48k with dynamic microphone sample rate
+        ind = ind + 1;
     end
+    ind = ind + 1;
 end
 
 timediff
