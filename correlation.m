0b4583c1514bfceeeb3ebffbe6b8ba06b268a32f
Taylor Mattison <taylorjm@vt.edu>
2015-04-09 13:14:06 -0400

    Functioning TDOA

---
+++ correlation.m
@@ -0,0 +1,9 @@
+function [corr, lag] = correlation(a,b)
+%CORRELATION calculates the cross-correlation between a and b
+%http://stackoverflow.com/questions/7396814/cross-correlation-in-matlab-without-using-the-inbuilt-function
+%Duplicates the xcorr() function without using the MatLAB signal processing toolbox
+
+corrLength = length(a) + length(b) - 1;
+corr = fftshift(ifft(fft(a, corrLength) .* conj(fft(b, corrLength))));
+lag = -(length(a) - 1) : length(a) - 1;
+end
