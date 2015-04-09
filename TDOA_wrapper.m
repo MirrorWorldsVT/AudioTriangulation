461b0cff929be8641be46c956cc4eb3e66433a40
Taylor Mattison <taylorjm@vt.edu>
2015-04-09 13:25:26 -0400

    Working

--- TDOA_wrapper.m
+++ TDOA_wrapper.m
@@ -3,23 +3,22 @@ function [ output_array ] = TDOA_wrapper( input_array )
 %   Detailed explanation goes here
 [numSamples, n] = size(input_array);
 
-%n*(n-1)/2
-timediff = zeros(n*(n-1)/2 , 1);
-corr = zeros(n*(n-1)/2 , 1); 
+timediff = zeros(n*(n-1)/2, 1);
+corr = zeros(n*(n-1)/2 , 1);
 lag = zeros(n*(n-1)/2 , 1);
+count = 1;
+
+ind = 1;
 
 for i = 1:(n-1)
     for j = (i+1):n
         audio_one = input_array(:,i);
         audio_two = input_array(:,j);
-        [timediff(i+(j-1)), corr(i+(j-1)), lag(i+(j-1))] = TDOA(audio_one, audio_two, 48000);
-        %replace 48k with dynamic microphone sample rate
+        [timediff(count), ~, ~] = TDOA(audio_one, audio_two, 48000);
+        count = count + 1;
     end
 end
 
-timediff
-corr
-lag
+output_array = timediff;
 
 end
-
