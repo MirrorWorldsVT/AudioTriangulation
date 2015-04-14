16cc39f9fd1c9900191c7e250ec0fe97b613507a
Taylor Mattison <taylorjm@vt.edu>
2015-04-14 10:40:45 -0400

    Live biangulation

--- MultiRecord.m
+++ MultiRecord.m
@@ -1,6 +1,6 @@
 UsingTestPlatform = false; %Remember to use ASIO for non-test platform
 CreateOutputFile = false;
-TimeToRecord = 5;
+TimeToRecord = 30;
 
 if (CreateOutputFile)
     AFW = dsp.AudioFileWriter('MultiRecordOut.wav','FileFormat', 'WAV');
@@ -8,11 +8,11 @@ end
 H = dsp.AudioRecorder;
 H.QueueDuration = 2;
 H.OutputNumOverrunSamples = true;
-H.SamplesPerFrame = 4000;
+H.SamplesPerFrame = 48000;
 
 if UsingTestPlatform == false
     H.SampleRate = 48000; %Our Audio-Technica mics record at 48000
-    H.NumChannels = 4; %Set to number of mics
+    H.NumChannels = 2; %Set to number of mics
 else
     disp('Using Test');
 end
@@ -27,6 +27,13 @@ while toc < TimeToRecord
   if (CreateOutputFile)
     step(AFW,audioIn);
   end
+  
+  time_diff = TDOA_wrapper(audioIn);
+  %disp(time_diff*340*39.3701) %Time difference in inches
+  disp((221-(time_diff*340.29*39.3701))/2) %Distance from 1 microphone on a line in a 2 mic system
+  %plot(audioIn)
+  %drawnow
+  
   totalRecording(H.SamplesPerFrame*count+1:H.SamplesPerFrame*count+H.SamplesPerFrame, :) = audioIn;
   if nOverrun > 0
     fprintf('Audio recorder queue was overrun by %d samples\n'...
